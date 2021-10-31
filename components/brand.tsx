36b43e0774dce93d59f103c93a569be3cf3e2e60
Lemasc <lemascserviceth@hotmail.com>
2021-10-31 18:41:23 +0700

    Use netlify plugin v4

--- components/brand.tsx
+++
@@ -1,9 +0,0 @@
-export default function Brand({ className, landing }: { className: string; landing: boolean }) {
-  return (
-    <h1 className={`${className} header-font select-none`}>
-      <span className={landing ? "text-white" : "text-red-500"}>เรียน</span>
-      <span className={landing ? "text-white" : "text-purple-500"}>อะไร</span>
-      <span className="text-lg pl-2">3.0</span>
-    </h1>
-  );
-}
